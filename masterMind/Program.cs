710befcc88f9d920c05b4dd08f4eac7b031f3aa0
Anthony Perez <antrp93@gmail.com>
2019-10-30 18:21:34 -0500

    Built masterMind, not fully completed as of yet

--- masterMind/Program.cs
+++ masterMind/Program.cs
@@ -7,29 +7,48 @@ namespace masterMind
     {
         static List<string> colors = new List<string> { "Red", "Yellow", "Blue" };
         static Random compSelect = new Random();
+        static string UserGuess = Console.ReadLine();
+
+        static int guessCompare = 0;
+        static bool WinCheck = false;
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Mastermind!\nThe computer will randomly choose 2 out of 3 colors.\nRed, Yellow, or Blue");
+            Console.WriteLine("Welcome to Mastermind!\n \nThe computer will randomly choose 2 out of 3 colors.\n \nRed, Yellow, or Blue\n \nPlease enter in your guess: ");
             int colorIndex = compSelect.Next(colors.Count);
             var color1 = colors[colorIndex];
             colorIndex = compSelect.Next(colors.Count);
             var color2 = colors[colorIndex];
-            Console.WriteLine("Enter in your guess");
-            Console.ReadLine();
-            //Console.WriteLine($"The colors randomly chosen are {color1} and {color2}");
+
+            string CPUcolors = color1 + " " + color2;
+
+            //Console.WriteLine("Enter in your guess:");
 
 
+            while (UserGuess != CPUcolors)
+            {
+                guessCompare = string.Compare(UserGuess, CPUcolors);
+                Console.WriteLine("Enter in your guess:");
+                UserGuess = Console.ReadLine();
 
 
+                if (WinCheck == true)
+                {
+                    Console.WriteLine("You win!");
+                    Console.ReadLine();
 
+                }
+            }
         }
 
 
-
-
-
-
-
     }
+
+
+
+
+
+
+
 }
