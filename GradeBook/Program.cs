3672e3532e575e9cb466fc5ebb5dd16a036e3bc6
Anthony Perez <antrp93@gmail.com>
2019-10-31 15:27:40 -0500

    added upon GradeBook Program

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -10,6 +10,7 @@ namespace GradeBook
             string answer = string.Empty;
             string quitProgram = string.Empty;
             Dictionary<string, string> gradeBook = new Dictionary<string, string>();
+            string[] seperator = { " " };
 
             do
             {
@@ -25,15 +26,22 @@ namespace GradeBook
                 answer = Console.ReadLine();
             } while (answer != "c");
 
-            foreach (var studentEntry in gradeBook)
+            foreach (KeyValuePair<string,string> entry in gradeBook)
             {
+
+                Console.WriteLine("Student: {0}, Grades: {1}", entry.Key, entry.Value);
+
+
                 var name = gradeBook.Keys;
                 var grade = gradeBook.Values;
 
-                Console.WriteLine(name);
-                Console.WriteLine(grade.Count);
-                
+               
+
             }
+
+            
+
+
         }
     }
 }
