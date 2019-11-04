ec95ba285f4163470839f1e75932f425b5edce04
Anthony Perez <antrp93@gmail.com>
2019-11-04 00:16:00 -0600

    Finished masterMind program

--- masterMind/Program.cs
+++ masterMind/Program.cs
@@ -1,50 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace masterMind
 {
     internal class Program
     {
         private static List<string> colors = new List<string> { "Red", "Yellow", "Blue" };
-        private static Random compSelect = new Random();
-        private static string UserGuess = Console.ReadLine();
+        private static Random CPUselect = new Random();
 
-        private static int guessCompare = 0;
-        private static bool WinCheck = true;
+        private static bool WinCheck = false;
 
         private static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Mastermind!\n \nThe computer will randomly choose 2 out of 3 colors.\n \nRed, Yellow, or Blue\n \nPlease enter in your guess: ");
-            int colorIndex = compSelect.Next(colors.Count);
+            Console.WriteLine("Welcome to Mastermind!\n \nThe computer will randomly choose 2 out of 3 colors.\n \nRed, Yellow, or Blue\n\nIt is up to YOU to figure out what the computer chose!\n\nPlease enter in your guess: ");
+            int colorIndex = CPUselect.Next(colors.Count);
             var color1 = colors[colorIndex];
-            colorIndex = compSelect.Next(colors.Count);
+            colorIndex = CPUselect.Next(colors.Count);
             var color2 = colors[colorIndex];
 
-            string CPUcolors = color1 + " " + color2;
+            string[] CPUcolors = { color1, color2 };
+            string[] UserGuess = Console.ReadLine().Split(" ");
 
-            //Console.WriteLine("Enter in your guess:");
-            guessCompare = string.Compare(UserGuess, CPUcolors, true);
-
-            if (WinCheck == true)
+            while (!WinCheck)
             {
-                Console.WriteLine("You win!");
-                Console.ReadLine();
+                UserGuess.SequenceEqual(CPUcolors, StringComparer.CurrentCultureIgnoreCase);
+                if (UserGuess.Contains("red") || UserGuess.Contains("yellow") || UserGuess.Contains("blue") || UserGuess.Length < 0)
+                {
+                    Console.WriteLine("\nPlease be sure you are using proper casing and be sure to check spelling.\nYou must enter in 2 colors, seperated by spaces, from the list\nPlease try again.\n");
+                    UserGuess = Console.ReadLine().Split(" ");
+                }
+                else if (UserGuess[0] != CPUcolors[0] && UserGuess[1] != CPUcolors[1] && UserGuess[0] != CPUcolors[1] && UserGuess[1] != CPUcolors[0])
+                {
+                    Console.WriteLine("\nYour hint: 0-0\n \nTry again");
+                    UserGuess = Console.ReadLine().Split(" ");
+                }
+                else if (UserGuess[0] == CPUcolors[1] && UserGuess[1] != CPUcolors[0])
+                {
+                    Console.WriteLine("\nYour hint: 1-0\n \nTry again");
+                    UserGuess = Console.ReadLine().Split(" ");
+                }
+                else if (UserGuess[0] == CPUcolors[0] && UserGuess[1] != CPUcolors[1] || UserGuess[1] == CPUcolors[1] && UserGuess[0] != CPUcolors[0])
+                {
+                    Console.WriteLine("\nYour hint: 0-1\n \nTry again");
+                    UserGuess = Console.ReadLine().Split(" ");
+                }
+                else if (UserGuess[0] == CPUcolors[1] && UserGuess[1] == CPUcolors[0])
+                {
+                    Console.WriteLine("\nYour hint: 2-0\n \nTry again");
+                    UserGuess = Console.ReadLine().Split(" ");
+                }
+                else
+                {
+                    WinCheck = true;
+                    Console.WriteLine("\nYou Win! Time to eat some cake!");
+                    Console.ReadLine();
+                }
             }
-            else
-            {
-                Console.WriteLine("Sorry, try again");
-                //hints go here, need to figure out how hint logic is to be represented
-            }
-
-            do
-            {
-                Console.WriteLine("Enter in your guess:");
-                UserGuess = Console.ReadLine();
-
-                Console.WriteLine(guessCompare);
-            }
-            while (UserGuess != CPUcolors);
-
         }
     }
 }
